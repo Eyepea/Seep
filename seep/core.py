54cdce90a2714802daf33e3b4a21a13639a95bd2
Quentin Dawans <quentin.dawans@eyepea.eu>
2017-01-06 09:25:12 +0100

    Change validation when proprety is required with or without default

--- seep/core.py
+++ seep/core.py
@@ -28,6 +28,7 @@ def extend(validator_cls):
     Validator = jsonschema.validators.extend(
         validator_cls, {
             "properties" : _properties_with_defaults(validator_cls),
+            "required": _properties_required(validator_cls)
         }
     )
 
@@ -61,12 +62,33 @@ def _properties_with_defaults(validator_cls):
 
         subschemas = [(instance, schema)]
         while subschemas:
+            to_del = [key for key, value in instance.items() if
+                      isinstance(value, SeepDict)]
+            for key in to_del:
+                del instance[key]
             subinstance, subschema = subschemas.pop()
             DefaultSetter(subschema).validate(subinstance)
-            subschemas.extend(
-                (subinstance.setdefault(property, {}), subsubschema)
-                for property, subsubschema in
-                subschema.get("properties", {}).items()
-            )
+            subschemas.extend((subinstance.setdefault(property,
+                                                      SeepDict()),
+                               subsubschema) for property, subsubschema
+                              in subschema.get("properties", {}).items())
 
     return properties_with_defaults
+
+
+def _properties_required(validator_cls):
+    def properties_required(validator, properties, instance, schema):
+
+        if 'default' in schema['properties'][properties[0]]:
+            pass
+        else:
+            for error in validator_cls.VALIDATORS["required"](
+                    validator, properties, instance, schema
+            ):
+                yield error
+
+    return properties_required
+
+
+class SeepDict(dict):
+    pass
